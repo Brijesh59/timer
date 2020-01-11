646bb8bff833c8b951e3788106f5b4cb64c60bce
Brijesh Kumar <kumarbrijesh9021@gmail.com>
2020-01-11 13:16:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ let timer = setInterval(function() {
     let distance = todayDate - lastMeetDate;
   
     // Time calculations for days, hours, minutes and seconds
+    // let months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let milliSeconds = (days * 3600 +  hours * 60 + seconds) * 10
   
     // Display the result in the element with id="demo"
-    document.getElementById("day").innerHTML = days
-    document.getElementById("hr").innerHTML = hours
-    document.getElementById("min").innerHTML = minutes
-    document.getElementById("sec").innerHTML = seconds
+    let dy = document.getElementById("day")
+    console.log("html: ", + dy.innerHTML)
+    console.log("text: "  + dy.innerText)
+    document.getElementById("day").innerText = days 
+    document.getElementById("hr").innerHTML = hours 
+    document.getElementById("min").innerHTML = minutes 
+    document.getElementById("sec").innerHTML = seconds 
+    // document.getElementById("milli").innerHTML = milliSeconds 
 
     // if (distance < 0) {
     //   clearInterval(timer);
